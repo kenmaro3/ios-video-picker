b31b7bc4db9fd0665da38342ee8a49cd2805098d
kenmaro3 <miha.ken.19@gmail.com>
2023-08-14 15:24:30 +0900

    Initial Commit

---
+++ VideoPicker/VideoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPickerApp.swift
+//  VideoPicker
+//
+//  Created by Kentaro Mihara on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct VideoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
